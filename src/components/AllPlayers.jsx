957ba8cf7510aa49487ee8c01f2a089ae482d15f
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-02-29 12:49:28 -0500

    updates

--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -7,6 +7,16 @@ function AllPlayers() {
             <h2>All Players Component</h2>
             <NewPlayerForm />
             <h3>Roster</h3>
+
+            {dummyPlayers.map((player) => (
+                <div key={player.id} className="player-card">
+                    <h4>{player.name}</h4>
+                    <p>{player.breed}</p>
+                    <img src={player.imageUrl} alt='player image'></img>
+                    <button>View Details</button>
+                    <button>Remove</button>
+                </div>
+            ))}
         </>
     );
 }
