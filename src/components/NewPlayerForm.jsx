01e8170c81b1fd8c0394cc33b7f75ac7c0a77e77
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-02-29 12:57:45 -0500

    form updates

--- src/components/NewPlayerForm.jsx
+++ src/components/NewPlayerForm.jsx
@@ -1,7 +1,19 @@
 function NewPlayerForm() {
     return (
         <>
-            <h2>NewPlayerForm Component</h2>
+            <h3>Add a new player</h3>
+            <form>
+                <label>
+                    Name: <input type='text' name='player-name' id='player-name' placeholder="Lucky" />
+                </label>
+                <label>
+                    Breed: <input type='text' name='player-breed' id='player-breed' placeholder="Corgi" />
+                </label>
+                <label>
+                    Image Url: <input type='text' name='player-image' id='player-image' placeholder="image url here" />
+                </label>
+                <button>Submit</button>
+            </form>
         </>
     );
 }
